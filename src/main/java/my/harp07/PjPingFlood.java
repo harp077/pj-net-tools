404fc4f1bcc6f335c81c8966a4e737a163590987
Roman Koldaev <harp07@mail.ru>
2022-04-18 19:24:30 +0400

    code optimize

--- src/main/java/my/harp07/PjPingFlood.java
+++ src/main/java/my/harp07/PjPingFlood.java
@@ -45,7 +45,7 @@ public class PjPingFlood {
                             }
                         });
                 end = System.currentTimeMillis();
-                if (k % 15 == 0) {
+                if (k % 13 == 0) {
                     ta.append("\n-> " + 1000 * (M + N) / (end - run) + " pps");
                 } else {
                     ta.append(", -> " + 1000 * (M + N) / (end - run) + " pps");
